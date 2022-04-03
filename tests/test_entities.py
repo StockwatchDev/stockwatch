53d2a0587f28741f26a1dd2355091d9f95387a83
Theo de Vries <t.j.a.devries@gmail.com>
2022-04-03 22:36:25 +0200

    added tests

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -1,6 +1,13 @@
-import pytest
+# pylint: skip-file
 from datetime import date, timedelta
-from stockwatch.entities import SharePosition, SharePortfolio
+import pytest
+from stockwatch.entities import (
+    SharePosition,
+    SharePortfolio,
+    closest_portfolio_after_date,
+    closest_portfolio_before_date,
+    get_all_isins,
+)
 
 
 @pytest.fixture
@@ -28,6 +35,33 @@ def example_portfolio_1() -> SharePortfolio:
         date.today(),
     )
     return SharePortfolio((sp1, sp2), date.today())
+
+
+@pytest.fixture
+def example_portfolio_2() -> SharePortfolio:
+    sp1 = SharePosition(
+        "VanEck Sustainable World Equal Weight UCITS ETF",
+        "NL0010408704",
+        "EUR",
+        1000.00,
+        36,
+        28.76,
+        1035,
+        86.50,
+        date.today() - timedelta(days=21),
+    )
+    sp2 = SharePosition(
+        "Vanguard FTSE All-World UCITS ETF USD Dis",
+        "IE00B3RBWM25",
+        "EUR",
+        970.00,
+        10,
+        104.23,
+        1042.30,
+        23.66,
+        date.today() - timedelta(days=21),
+    )
+    return SharePortfolio((sp1, sp2), date.today() - timedelta(days=21))
 
 
 def test_value(example_portfolio_1: SharePortfolio) -> None:
@@ -65,3 +99,40 @@ def test_isins_names(example_portfolio_1: SharePortfolio) -> None:
 
 def test_is_date_consistent(example_portfolio_1: SharePortfolio) -> None:
     assert example_portfolio_1.is_date_consistent()
+
+
+def test_closest_portfolio(
+    example_portfolio_1: SharePortfolio, example_portfolio_2: SharePortfolio
+) -> None:
+    portfolio_set = (example_portfolio_2, example_portfolio_1)
+    assert (
+        closest_portfolio_after_date(portfolio_set, date.today() + timedelta(days=1))
+        == None
+    )
+    assert (
+        closest_portfolio_after_date(portfolio_set, date.today() - timedelta(days=1))
+        == example_portfolio_1
+    )
+    assert (
+        closest_portfolio_before_date(portfolio_set, date.today() + timedelta(days=1))
+        == example_portfolio_1
+    )
+    assert (
+        closest_portfolio_before_date(portfolio_set, date.today() - timedelta(days=1))
+        == example_portfolio_2
+    )
+    assert (
+        closest_portfolio_before_date(portfolio_set, date.today() - timedelta(days=50))
+        == None
+    )
+
+
+def test_get_all_isins(
+    example_portfolio_1: SharePortfolio, example_portfolio_2: SharePortfolio
+) -> None:
+    portfolio_set = (example_portfolio_2, example_portfolio_1)
+    all_isins = get_all_isins(portfolio_set)
+    assert len(all_isins) == 3
+    assert "NL0010408704" in all_isins
+    assert "IE00B441G979" in all_isins
+    assert "IE00B3RBWM25" in all_isins
