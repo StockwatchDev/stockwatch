209f7762750e37d78a06323b7fe3ce9e15083c45
Theo de Vries <t.j.a.devries@gmail.com>
2022-08-18 14:33:05 +0200

    remove useless class

--- src/stockwatch/use_cases/configuring.py
+++ src/stockwatch/use_cases/configuring.py
@@ -49,7 +49,7 @@ class Config(ConfigBase):
             sections: dict[str, Any] = {}
             for f in _config_fields:
                 section_name = f.name
-                section = DataClassUnpack.instantiate(
+                section = instantiate_config(
                     section_name, stockwatch_config_stored[section_name]
                 )
                 sections[section_name] = section
@@ -62,24 +62,20 @@ class Config(ConfigBase):
         Config._instance = self
 
 
-class DataClassUnpack:  # pylint: disable=too-few-public-methods
-    """Helper class: initialize dataclasses from dicts that are filtered for known fields"""
-
-    @classmethod
-    def instantiate(
-        cls, classname_to_instantiate: str, arg_dict: dict[str, Any]
-    ) -> ConfigBase | None:
-        """Return an instance of classname_to_instantiate, properly initialized"""
-        if not (
-            class_to_instantiate := getattr(
-                sys.modules[__name__], f"{classname_to_instantiate}Config"
-            )
-        ):
-            print(f"No Config class known for section named {classname_to_instantiate}")
-            return None
-        field_set = {f.name for f in fields(class_to_instantiate) if f.init}
-        filtered_arg_dict = {k: v for k, v in arg_dict.items() if k in field_set}
-        return class_to_instantiate(**filtered_arg_dict)  # type: ignore
+def instantiate_config(
+    classname_to_instantiate: str, arg_dict: dict[str, Any]
+) -> ConfigBase | None:
+    """Return an instance of classname_to_instantiate, properly initialized"""
+    if not (
+        class_to_instantiate := getattr(
+            sys.modules[__name__], f"{classname_to_instantiate}Config"
+        )
+    ):
+        print(f"No Config class known for section named {classname_to_instantiate}")
+        return None
+    field_set = {f.name for f in fields(class_to_instantiate) if f.init}
+    filtered_arg_dict = {k: v for k, v in arg_dict.items() if k in field_set}
+    return class_to_instantiate(**filtered_arg_dict)  # type: ignore
 
 
 def get_config() -> Config:
