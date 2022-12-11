32972392b6758a2f19b2f300be219538739fe80d
Theo de Vries <3439143+tjadevries@users.noreply.github.com>
2022-12-11 22:14:02 +0100

    add and improve operators

--- src/stockwatch/entities2/currencies.py
+++ src/stockwatch/entities2/currencies.py
@@ -26,20 +26,38 @@ class Amount:
     def __post_init__(self) -> None:
         object.__setattr__(self, "value", round(self.value_exact, 2))
 
+    def __eq__(self, other: object) -> bool:
+        "Return true if currencies match and self.value equals other.value (i.e., the rounded values)"
+        if not isinstance(other, Amount):
+            raise NotImplementedError
+        return self.curr == other.curr and self.value == other.value
+
     def __add__(self, other: Amount) -> Amount:
         "Return an amount self.value + other.value"
         assert self.curr == other.curr
-        return replace(self, value_exact=self.value + other.value)
+        return replace(self, value_exact=self.value_exact + other.value_exact)
 
     def __sub__(self, other: Amount) -> Amount:
-        "Return an amount self.value + other.value"
+        "Return an amount self.value - other.value"
         assert self.curr == other.curr
-        return replace(self, value_exact=self.value - other.value)
+        return replace(self, value_exact=self.value_exact - other.value_exact)
 
     def __neg__(self) -> Amount:
         "Return an amount with negated self.value"
         return replace(self, value_exact=-self.value_exact)
 
+    def __pos__(self) -> Amount:
+        "Return an amount with self.value"
+        return self
+
     def __mul__(self, factor: float) -> Amount:
         "Return an amount self.value_exact * factor"
         return replace(self, value_exact=(self.value_exact * factor))
+
+    def __rmul__(self, factor: float) -> Amount:
+        "Return an amount self.value_exact * factor"
+        return replace(self, value_exact=(self.value_exact * factor))
+
+    def __truediv__(self, divisor: float) -> Amount:
+        "Return an amount self.value_exact * (1.0/divisor)"
+        return replace(self, value_exact=(self.value_exact * (1.0 / divisor)))
