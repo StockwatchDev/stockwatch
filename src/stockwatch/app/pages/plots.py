0398fbdcc183f0ff9fe2227aa04253fd7107f6b6
Jorik de Vries <jorik.de.vries@gmail.com>
2022-12-30 18:07:38 +0100

    Add filtering on start and end dates to the plots.

--- src/stockwatch/app/pages/plots.py
+++ src/stockwatch/app/pages/plots.py
@@ -1,24 +1,100 @@
 """All dash specific callbacks and layout related to the plots page."""
+# pylint: disable=duplicate-code
+from datetime import date, timedelta
+
 import dash
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 
-from stockwatch import analysis, entities, use_cases
-from stockwatch.app import ids
-
-_PORTOS: tuple[entities.shares.SharePortfolio, ...] = ()
-_INDICES: list[tuple[entities.shares.SharePosition, ...]] = []
+from stockwatch import analysis, entities
+from stockwatch.app import ids, runtime
 
 
 def _get_layout() -> dash.html.Div:
     """Get the layout."""
     return dash.html.Div(
         [
-            dash.html.Button(
-                "Refresh", id=ids.PlottingId.REFRESH, n_clicks=0, hidden=True
-            ),
             dbc.Container(
                 [
+                    dbc.Row(
+                        [
+                            dbc.Col(
+                                dash.html.Label("Start date:"),
+                                width={
+                                    "size": 2,
+                                    "offset": 1,
+                                },
+                                xl=1,
+                                style={"float": "right"},
+                            ),
+                            dbc.Col(
+                                dash.dcc.DatePickerSingle(
+                                    id=ids.PlottingId.START_DATE,
+                                    min_date_allowed=date(2008, 1, 1),
+                                    max_date_allowed=date.today(),
+                                    initial_visible_month=date.today(),
+                                    display_format="DD/MM/YYYY",
+                                    className="dbc",
+                                    number_of_months_shown=3,
+                                    clearable=True,
+                                ),
+                                width="auto",
+                                lg=2,
+                            ),
+                            dbc.Col(
+                                dash.html.Label("End date:"),
+                                width=2,
+                                xl=1,
+                                style={"float": "right"},
+                            ),
+                            dbc.Col(
+                                dash.dcc.DatePickerSingle(
+                                    id=ids.PlottingId.END_DATE,
+                                    min_date_allowed=date(2008, 1, 1),
+                                    max_date_allowed=date.today(),
+                                    initial_visible_month=date.today(),
+                                    display_format="DD/MM/YYYY",
+                                    className="dbc",
+                                    number_of_months_shown=3,
+                                    clearable=True,
+                                ),
+                                width="auto",
+                                lg=2,
+                            ),
+                        ],
+                        align="center",
+                        className="mb-3",
+                    ),
+                    dbc.Row(
+                        [dbc.Col(width=1)]
+                        + [
+                            dbc.Col(
+                                dbc.Button(
+                                    title,
+                                    n_clicks=0,
+                                    id=btn_id,
+                                    color="dark",
+                                    outline=btn_id is ids.PlottingId.REFRESH,
+                                ),
+                                width=1,
+                                className="d-grid gap-2",
+                            )
+                            for title, btn_id in (
+                                ("Year to Date", ids.PlottingId.YTD_BTN),
+                                ("Last Year", ids.PlottingId.LY_BTN),
+                                ("This Month", ids.PlottingId.TM_BTN),
+                                ("Last Month", ids.PlottingId.LM_BTN),
+                                ("Clear", ids.PlottingId.CLEAR_BTN),
+                                ("Refresh", ids.PlottingId.REFRESH),
+                            )
+                        ],
+                        justify="start",
+                        className="mb-3",
+                    ),
+                    dbc.Row(
+                        dbc.Col(dash.html.Hr()),
+                        className="mb-3",
+                    ),
                     dbc.Row(
                         dbc.Col(
                             _create_graph_card(
@@ -47,8 +123,11 @@ def _create_graph_card(title: str, graph_id: ids.PlottingId) -> dbc.Card:
         [
             dbc.CardHeader(
                 dash.html.H2(
-                    title, style={"text-align": "center"}, className="card-title"
-                )
+                    title,
+                    style={"textAlign": "center", "color": "#ECECEC"},
+                    className="card-title",
+                ),
+                class_name="bg-dark",
             ),
             dbc.CardBody(
                 [
@@ -75,32 +154,91 @@ layout = _get_layout()
     dash.State(ids.PlottingId.REFRESH, "n_clicks"),
 )
 def _update_portfolios(_: int, refresh_clicks: int) -> int | dash._callback.NoUpdate:
-    global _PORTOS  # pylint: disable=global-statement
-    global _INDICES  # pylint: disable=global-statement
-
-    _PORTOS, _INDICES = use_cases.get_portfolios_index_positions()
-
+    runtime.load_portfolios()
     return refresh_clicks + 1
 
 
 @dash.callback(
     dash.Output(ids.PlottingId.GRAPH_RESULT, "figure"),
+    dash.State(ids.PlottingId.START_DATE, "date"),
+    dash.State(ids.PlottingId.END_DATE, "date"),
     dash.Input(ids.PlottingId.REFRESH, "n_clicks"),
 )
-def _draw_portfolio_graph(_clicks: int) -> dash.dcc.Graph:
-    if not _PORTOS:
+def _draw_portfolio_graph(
+    start_date_str: str | None, end_date_str: str | None, _clicks: int
+) -> dash.dcc.Graph | go.Figure:
+    start_date = date.fromisoformat(start_date_str) if start_date_str else None
+    end_date = date.fromisoformat(end_date_str) if end_date_str else None
+
+    if not (portos := runtime.get_portfolios(start_date, end_date)):
         return go.Figure()
-    return analysis.plot_positions(_PORTOS)
+    return analysis.plot_positions(portos)
 
 
 @dash.callback(
     dash.Output(ids.PlottingId.GRAPH_TOTAL, "figure"),
+    dash.State(ids.PlottingId.START_DATE, "date"),
+    dash.State(ids.PlottingId.END_DATE, "date"),
     dash.Input(ids.PlottingId.REFRESH, "n_clicks"),
 )
-def _draw_portfolio_graph_total(_clicks: int) -> dash.dcc.Graph:
-    if not _PORTOS:
+def _draw_portfolio_graph_total(
+    start_date_str: str | None, end_date_str: str | None, _clicks: int
+) -> dash.dcc.Graph | go.Figure:
+    start_date = date.fromisoformat(start_date_str) if start_date_str else None
+    end_date = date.fromisoformat(end_date_str) if end_date_str else None
+
+    if not (portos := runtime.get_portfolios(start_date, end_date)):
         return go.Figure()
 
     index_positions: list[tuple[entities.shares.SharePosition, ...]] = []
 
-    return analysis.plot_returns(_PORTOS, index_positions)
+    return analysis.plot_returns(portos, index_positions)
+
+
+@dash.callback(
+    [
+        dash.Output(ids.PlottingId.START_DATE, "date"),
+        dash.Output(ids.PlottingId.END_DATE, "date"),
+    ],
+    [
+        dash.Input(ids.PlottingId.LY_BTN, "n_clicks"),
+        dash.Input(ids.PlottingId.YTD_BTN, "n_clicks"),
+        dash.Input(ids.PlottingId.TM_BTN, "n_clicks"),
+        dash.Input(ids.PlottingId.LM_BTN, "n_clicks"),
+        dash.Input(ids.PlottingId.CLEAR_BTN, "n_clicks"),
+    ],
+)
+def _update_start_end_date(
+    _n_clicks_ly: int,
+    _n_clicks_ytd: int,
+    _n_clicks_tm: int,
+    _n_clicks_lm: int,
+    _n_clicks_clear: int,
+) -> tuple[date | None, date | None] | dash._callback.NoUpdate:
+    start_date: date | None = date.today()
+    end_date: date | None = date.today()
+    match dash.callback_context.triggered_id:
+        case None:
+            return dash.no_update
+        case ids.PlottingId.CLEAR_BTN:
+            start_date = None
+            end_date = None
+        case ids.PlottingId.YTD_BTN:
+            end_date = date.today()
+            start_date = date(year=end_date.year, month=1, day=1)
+        case ids.PlottingId.TM_BTN:
+            end_date = date.today()
+            start_date = date(year=end_date.year, month=end_date.month, day=1)
+        case ids.PlottingId.LY_BTN:
+            today = date.today()
+            start_date = date(year=today.year - 1, month=1, day=1)
+            end_date = date(
+                year=today.year - 1,
+                month=12,
+                day=31,
+            )
+        case ids.PlottingId.LM_BTN:
+            today = date.today()
+            end_date = today - timedelta(days=today.day + 1)
+            start_date = date(year=end_date.year, month=end_date.month, day=1)
+    return start_date, end_date
